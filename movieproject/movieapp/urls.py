378a9a9a2ed4b7e0ced6024b1cf755e931beec53
sreerajpssree <sreerajps8@gmail.com>
2024-01-11 11:26:39 +0530

    added

--- movieproject/movieapp/urls.py
+++ movieproject/movieapp/urls.py
@@ -4,7 +4,7 @@ app_name = 'movieapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('movie/<int:movie_id>/', views.detail, name='detail'),
-    path('add/',views.add_movie, name='add_movie'),
+    path('add/', views.add_movie, name='add_movie'),
     path('update/<int:id>/', views.update, name='update'),
     path('delete/<int:id>/', views.delete, name='delete')
 ]
